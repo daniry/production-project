c61bb27b8f1bb45b1c2bbde4937f9947c9776274
dasharubakova <dasharubakova@yandex.ru>
2025-09-03 16:15:32 +0300

    fix tests & add visual tool

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,11 +1,11 @@
 import { fireEvent, screen } from '@testing-library/react';
-import { renderWithTranslation }
-    from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender }
+    from 'shared/lib/tests/componentRender/componentRender';
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar', () => {
     test('test render', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         // render(
         //     <I18nextProvider i18n={i18n}>
         //         <Sidebar />
@@ -16,7 +16,7 @@ describe('Sidebar', () => {
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
     test('test toggle', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
